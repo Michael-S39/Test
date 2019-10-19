0be91e5de884493bacb9992561c0a76c99a93673
Michael <alfa777@yandex.ru>
2019-10-19 13:45:49 +0200

    Это наш первый коммит

---
+++ Testptoject/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//let ansver = prompt("Тебе уже 18?", "Да");
+//console.log(typeof(ansver)); 
+let incr = 10,
+decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
